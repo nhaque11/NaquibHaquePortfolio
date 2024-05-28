e83269490ad935cc5e51a374a9845258b833fd30
nhaque11 <99585088+nhaque11@users.noreply.github.com>
2024-05-27 21:00:46 -0700

    update stuff

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NaquibHaquePortfolio/",
+  base: "https://nhaque11.github.io/NaquibHaquePortfolio/",
   css: {
     modules: {
       localsConvention: "camelCase",
